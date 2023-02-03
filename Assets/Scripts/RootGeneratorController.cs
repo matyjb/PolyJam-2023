781a60c235c0df0e672e9d6376d286b376e5bcf7
matyjb <matyjasiakbartosz@gmail.com>
2023-02-03 23:11:30 +0100

    script cleanup

--- Assets/Scripts/RootGeneratorController.cs
+++ Assets/Scripts/RootGeneratorController.cs
@@ -6,21 +6,19 @@ using UnityEngine;
 [RequireComponent(typeof(TrailRenderer))]
 public class RootGeneratorController : MonoBehaviour
 {
-    public int maxLength = 20;
+    public float maxLength = 20;
     public float spawnAnotherChance = 0.01f;
-    public int minSegmentLength = 1;
-    public int maxSegmentLength = 5;
-    public int newBranchAngle = 30;
+    public float minSegmentLength = 1;
+    public float maxSegmentLength = 5;
+    public float newBranchAngle = 30;
     public float anglePerSecond = 50f;
     public float growthSpeed = 5;
     public float angleSpeed = 2;
     public float maxAngle = 90;
 
-    public float angle = 0;
-    public float segmentLen = 0;
+    float segmentLen = 0;
+    [HideInInspector]
     public float rootLen = 0;
-    public float rootAngle = 0;
-
 
     float randomAngleOffset = 0;
 
@@ -40,15 +38,9 @@ public class RootGeneratorController : MonoBehaviour
         rootLen += distance;
         transform.localPosition += step;
 
-        angle = Mathf.Sin(Time.time + randomAngleOffset) * anglePerSecond;
-        //angle += Mathf.Sin(Random.Range(0,2*Mathf.PI)) / 10;
-        //angle = Mathf.Min(angle, maxAngle);
-        //angle = Mathf.Max(angle, -maxAngle);
-
         if(Mathf.Abs(transform.localRotation.eulerAngles.z) <= maxAngle/2)
         {
-            float angleStep = angle * Time.deltaTime * angleSpeed;
-            rootAngle += angleStep;
+            float angleStep = Mathf.Sin(Time.time + randomAngleOffset) * Time.deltaTime * anglePerSecond;
             transform.Rotate(new Vector3(0, 0, 1), angleStep);
         }
         float rand = Random.Range(0, 1f);
