f3be67ac058cc1356a0e2d6453af89468100f460
Bionicl <maciejmaj@me.com>
2023-02-04 15:51:13 +0100

    Fake 2nd roots movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,18 +7,45 @@ public class Player : MonoBehaviour
     [Header("Prefabs")]
     public GameObject Root;
 
-    bool isMain = false;
+    public bool isMain = false;
+    public float fakeRotate = 0;
     public bool flipControls = false;
+    public float rotationSpeed;
     public Rigidbody2D rig2d;
     PlayerMovement playerMovement;
+
+    // Fake rotate
+    float fakeRotateTime = 0;
+    float nextFakeRotateStart;
+    float nextFakeRotateEnd;
+    float nextFakeRotateAmount;
 
     private void Awake() {
         rig2d = GetComponent<Rigidbody2D>();
     }
 
+    private void Update() {
+        if (isMain)
+            return;
+        fakeRotate = 0;
+        fakeRotateTime += Time.deltaTime;
+        if (fakeRotateTime > nextFakeRotateStart) {
+            fakeRotate = nextFakeRotateAmount;
+        }
+        if (fakeRotateTime > nextFakeRotateEnd) {
+            fakeRotateTime = 0;
+            QueueNewFakeRotate();
+        }
+        Debug.Log(fakeRotateTime + "; " + nextFakeRotateStart);
+    }
+
     public void Setup(bool isMain, PlayerMovement playerMovement) {
         this.isMain = isMain;
         this.playerMovement = playerMovement;
+        if (!isMain) {
+            QueueNewFakeRotate();
+            rotationSpeed = Random.Range(0.8f, 1.3f);
+        }
     }
 
     public void Split() {
@@ -37,5 +64,11 @@ public class Player : MonoBehaviour
         GameObject newGo = Instantiate(Root, playerMovement.transform);
         newGo.transform.position = transform.position;
         newGo.transform.eulerAngles = new Vector3(0, 0, 180);
+    }
+
+    void QueueNewFakeRotate() {
+        nextFakeRotateStart = Random.Range(0.3f, 1.1f);
+        nextFakeRotateEnd = nextFakeRotateStart + Random.Range(0.8f, 1.3f);
+        nextFakeRotateAmount = Random.Range(-3.2f, 3.2f);
     }
 }
