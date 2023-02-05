05c28e28ef537ed38cf6d16d861da3d0e6216c9d
matyjb <matyjasiakbartosz@gmail.com>
2023-02-05 11:57:37 +0100

    fix init pickups spawn

--- Assets/Scripts/ProximityPickupsGenerator.cs
+++ Assets/Scripts/ProximityPickupsGenerator.cs
@@ -55,7 +55,7 @@ public class ProximityPickupsGenerator : MonoBehaviour {
     }
 
     private void SpawnPickup(GameObject pickup, bool ignoreMinRadius = false) {
-        float distance = Random.Range(ignoreMinRadius ? 10f : minRadius, maxRadius);
+        float distance = Random.Range(ignoreMinRadius ? 3f : minRadius, maxRadius);
         float angle = Random.Range(minAngle, maxAngle);
 
         Vector3 spawnPoint = Helpers.RadianToCartesianCoords(distance, angle);
